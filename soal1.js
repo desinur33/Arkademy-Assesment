f3a6dd98af6ca765cc5ee62769b00b410baaa6dc
fajrulaulia <auliafajrul7@gmail.com>
2019-07-20 18:16:41 +0700

    Fix Soal 1

--- soal1.js
+++ soal1.js
@@ -1,26 +1,65 @@
 var MyData = {
   "name": 'Fajrul Aulia',
-  "address": 'Tanah kusir 2',
+  "age": 19,
+  "address": 'Desa Tanjung, Kecamatan Ingin Jaya, Kab Aceh Besar, Provinsi Aceh',
   "hobbies": [
-    'Ngoding', 'Fishing', 'Swimming' 
+    'Coding', 'Fishing', 'Swimming' 
   ],
   "is_married": false,
   "school":[
-      {
-        "SD": 'SDN 62 Banda Aceh',
-        "SMP": 'SMPN 14 Banda Aceh',
-        "SMA/K": 'SMKN 5 Telkom Banda Aceh',
-      }
-    ],
-  "skill": [
     {
-      "HTML5": "90 %",
-      "CSS": "80 %",
-      "Javascript": "95 %",
-      "Laravel": "35 %",
-      "Flutter": "35 %",
-      "Bootstrap": "90 %"
-    }
+      "name":'SDN 62 Banda Aceh',
+      "year_in":2005,
+      "year_out":2011,
+      "major":null
+    },
+    {
+          "name":'SMPN 14 Banda Aceh',
+          "year_in":2011,
+          "year_out":2014,
+          "major":null
+        },
+    {
+          "name":'SMKN 5 TELKOM Banda Aceh',
+          "year_in":2014,
+          "year_out":2017,
+          "major":"Rekaysa Perangkat Lunak"
+        },
+  ],
+  "skill":[
+    {
+      "name":"PHP",
+      "Level":"Expert"
+    },
+    {
+      "name":"JavaScript",
+      "Level":"Expert"
+    },
+    {
+      "name":"Laravel",
+      "Level":"Expert"
+    },
+    {
+      "name":"Flutter",
+      "Level":"Advanced"
+    },
+    {
+      "name":"Java",
+      "Level":"Advanced"
+    },
+    {
+      "name":"Python",
+      "Level":"Begineer"
+    },
+    {
+      "name":"MySQL",
+      "Level":"Expert"
+    },
+    {
+      "name":"HTML 5",
+      "Level":"Expert"
+    },
+
   ],
   "interest_in_coding":true
 }  
